66b5b6948f7bb515a4844642c551bddc7e5ec5e9
sa4801@CLASSROOM.NET <sa4801@CLASSROOM.NET>
2018-06-09 16:29:08 -0300

    Metodos de Delecao

--- src/main/java/br/com/impacta/controller/CartoesController.java
+++ src/main/java/br/com/impacta/controller/CartoesController.java
@@ -15,62 +15,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/cartoes")
 public class CartoesController {
 
-//	@RequestMapping(value="/inserir", method=RequestMethod.POST)  
-//	@ResponseBody
-//	public void inserir() {
-//		return entrada;
-//	}
-//
-//	@RequestMapping(value="/buscar", method=RequestMethod.GET)  
-//	@ResponseBody
-//	public Despesa buscar() {
-//		Despesa despesa = new Despesa();
-//		despesa.setCategoria(Categoria.ALIMENTACAO);
-//		despesa.setCodigo(1l);
-//		despesa.setData(new Date());
-//		despesa.setDescricao("descricao");
-//		despesa.setObservacoes("observacoes");
-//		despesa.setValor(new BigDecimal(123.45));
-//		return despesa;
-//	}
-//	
-//	@RequestMapping(value="/buscar2", 
-//			method=RequestMethod.GET,
-//			params = {"um", "dois"})  
-//	@ResponseBody
-//	public Despesa buscarMultiplosParametros(
-//			@RequestParam(value = "um") boolean um, 
-//			@RequestParam(value = "dois") boolean dois) {
-//		
-//		System.out.println("um: " + um);
-//		System.out.println("dois: " + dois);
-//		
-//		return new Despesa();
-//	}
-//	
-//	@RequestMapping(value="/buscarQualquerCoisa", 
-//			method=RequestMethod.GET,
-//			params = {"parametro"})  
-//	@ResponseBody
-//	public Despesa buscarQualquerCoisa(
-//			@RequestParam(value = "parametro") boolean parametro, 
-//			@RequestParam(value = "dois") boolean dois) {
-//		
-//		System.out.println("um: " + um);
-//		System.out.println("dois: " + dois);
-//		
-//		return new Despesa();
-//	}
-//
-//	public void atualizar() {
-//
-//	}
-//
-//	@RequestMapping(value="/deletar/{id}/{outra}", method=RequestMethod.DELETE)  
-//	@ResponseBody
-//	public boolean deletar(@PathVariable Long id, @PathVariable String outra) {
-//		System.out.println("deletando..." + id);
-//		System.out.println("deletando..." + outra);
-//		return false;
-//	}
+
+	@RequestMapping(value="/deletar/{id}",method=RequestMethod.DELETE)
+	@ResponseBody
+	public boolean deletar(@PathVariable long id) {
+		System.out.println("deletando id: "+id);
+		return false;
+	}
+
+	@RequestMapping(value = "/deletar", method = RequestMethod.DELETE)
+	@ResponseBody
+	public boolean deletar() {
+		System.out.println("deletando Tudo ");
+		return false;
+	}
 }
