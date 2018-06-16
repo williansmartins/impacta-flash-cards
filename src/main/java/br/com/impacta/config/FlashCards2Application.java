3c9c2490ef2d5a612ff18eea4c33f55d366f9f5d
Willians Martins <professorwillians@gmail.com>
2018-06-16 16:18:08 -0300

    add versao com spring boot

---
+++ src/main/java/br/com/impacta/config/FlashCards2Application.java
@@ -0,0 +1,12 @@
+package br.com.impacta.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCards2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCards2Application.class, args);
+	}
+}
---
+++ src/test/java/br/com/impacta/flashcards2/FlashCards2ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.impacta.flashcards2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlashCards2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
