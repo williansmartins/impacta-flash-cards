8e26f7c9a126e6180e351dac7e9f631f56884e42
Willians Martins <professorwillians@gmail.com>
2018-06-23 16:20:57 -0300

    habilitando CORS

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -2,8 +2,25 @@ var app = angular.module('myApp', []);
 
 
 app.controller('cartaoCtrl', function($scope, $http) {
-    $http.get("http://localhost:8080/cartoes")
-    .then(function (response) {
-    	$scope.cartoes = response.data;
-    });
+    
+    var buscarCartoes = function(){
+	    $http.get("http://localhost:8080/cartoes")
+	    .then(function (response) {
+	    	$scope.cartoes = response.data;
+	    });
+    }
+
+    var deletarCartao = function(id){
+	    $http.delete("http://localhost:8080/cartoes/deletar/" + id)
+	    .then(function (response) {
+	    	alert("sucesso");
+	    });
+    }
+
+    var init = function(){
+    	deletarCartao(34);  	
+    	buscarCartoes(); 
+    }
+
+    init();
 });
