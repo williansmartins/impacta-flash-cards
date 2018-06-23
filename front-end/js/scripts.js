4510b9f1d5f16271afc002ac576beccb185a5d1f
Willians Martins <professorwillians@gmail.com>
2018-06-23 15:43:27 -0300

    finalizando com Jquery

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -1,5 +1,20 @@
 $( document ).ready(function() {
-	$.get( "backend.txt", function( data ) {
-	  alert( "Data Loaded: " + data );
+	$.get( "http://localhost:8080/cartoes", function( data ) {
+	  //console.info( data );
+
+  	//$("#tabela #corpo").empty();
+	
+	for (var i = 0; i < data.length; i++) {
+		var cartao = data[i];
+		//console.info( cartao.id );
+		$("#tabela #corpo").append(" \
+			<tr> \
+				<td>" + cartao.id + "</td> \
+				<td>" + cartao.idiomaOrigem + "</td> \
+				<td>" + cartao.idiomaDestino + "</td> \
+			</tr> \
+		");
+	};
+
 	});
 });
